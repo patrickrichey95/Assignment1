5de2d636f8b4833d940b715a653e191076c685fa
Patrick Richey <patrick_richey@hotmail.com>
2014-09-27 20:36:54 -0400

    Finished contact form

---
+++ Assignment 1/send.php
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+		<title>Email Me</title>
+	</head>
+	<body>
+		<?php
+			//store the inputs in variables
+			$name = $_POST['name'];
+			$email = $_POST['email'];
+			$content = $_POST['content'];
+			$new_content = "From: ".$name."\n\n".$content;
+			
+			// use wordwrap() if lines are longer than 70 characters
+			$new_content = wordwrap($new_content,100);
+
+			mail('200257310@student.georgianc.on.ca' , 'Portfolio Inquiries', $new_content, "From: ".$email);
+			
+			header('Location: contact.html');
+		?>
+	</body>
+</html>
