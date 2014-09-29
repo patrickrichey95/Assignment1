4122e3161d93111db40686f1833b8db4a536f270
Patrick Richey <patrick_richey@hotmail.com>
2014-09-28 23:58:21 -0400

    Finish

--- Assignment 1/js/main.js
+++ Assignment 1/js/main.js
@@ -1,1 +1,8 @@
-﻿
+﻿/*
+    File: main.js
+    Author: Patrick Richey - 200257310
+    Website: Portfolio
+    This is the script file for my portfolio website, since I'm not very familiar with JavaScript, it appears to be fairly empty.
+    I managed to make my website work the way I wanted without JavaScript, but as I learn more about JavaScript it would be 
+    cool to add some elements to my page.
+*/
